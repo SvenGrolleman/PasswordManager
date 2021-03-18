31f9d515a295e91510a931752e04a1abcce78b29
Khesh <38070606+Khesh@users.noreply.github.com>
2021-03-18 17:29:32 +0100

    Emptied the CLI program.cs

--- PasswordManager.CLI/Program.cs
+++ PasswordManager.CLI/Program.cs
@@ -17,18 +17,6 @@ namespace PasswordManager.CLI
             var rfcArray = RfcEncryptor.HashWithSalt("secret".ToByteArrayFromString(), mainPw.MainSalt.ToByteArrayFromBase64());
             var verified = ByteArrayComparer.CompareByteArrays(rfcArray, mainPw.Password.ToByteArrayFromBase64());
             var key = RfcEncryptor.HashWithSalt("secret".ToByteArrayFromString(), mainPw.MainSalt.ToByteArrayFromBase64(), 1000);
-            //var IV = AESEncryptor.GetIV();
-            //var encryted = AESEncryptor.EncryptStringToBytes("test".ToByteArrayFromString(), key, IV);
-            //var entry = new PasswordEntry()
-            //{
-            //    Website = "test",
-            //    Username = "test",
-            //    IV = IV.ToBase64FromByteArray(),
-            //    Password = encryted.ToBase64FromByteArray(),
-            //};
-            //repository.InsertPasswordEntry(entry);
-            var entry = repository.GetPasswordEntry(2);
-            var decrypt = AESEncryptor.DecryptBytesToString(entry.Password.ToByteArrayFromBase64(), key, entry.IV.ToByteArrayFromBase64()).ToStringFromByteArray();
         }
     }
 }
