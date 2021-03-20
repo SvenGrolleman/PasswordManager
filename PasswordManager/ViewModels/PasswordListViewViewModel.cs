5f5ec05e3da26c78dc6dee4c8ca4c082f50f5b10
Sven Grolleman <stgrolleman@gmail.com>
2021-03-20 17:20:50 +0100

    Added a filter to the password entry list view model

--- PasswordManager/ViewModels/PasswordListViewViewModel.cs
+++ PasswordManager/ViewModels/PasswordListViewViewModel.cs
@@ -1,27 +1,51 @@
 ﻿using PasswordManager.Database;
-using PasswordManager.Encryption;
 using PasswordManager.EventsManager;
 using PasswordManager.HelperClasses;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace PasswordManager.Models
 {
-    public class PasswordListViewViewModel
+    public class PasswordListViewViewModel : INotifyPropertyChanged
     {
         private readonly IPwRepository _repository;
         private readonly PasswordManagerEventHandler _eventHandler;
+        private readonly ObservableCollection<PasswordListItemViewModel> _passwordsBackup = new ObservableCollection<PasswordListItemViewModel>();
 
-        public ObservableCollection<PasswordListItemViewModel> Passwords { get; private set; }
+        private ObservableCollection<PasswordListItemViewModel> _passwords;
+        public ObservableCollection<PasswordListItemViewModel> Passwords
+        {
+            get { return _passwords; }
+            set
+            {
+                if (_passwords != value)
+                {
+                    _passwords = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _filter;
+        public string Filter
+        {
+            get { return _filter; }
+            set
+            {
+                if (_filter != value)
+                {
+                    _filter = value;
+                    OnFilterChanged();
+                }
+            }
+        }
 
         public CommandBinder AddPasswordEntry { get; private set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        public PasswordListViewViewModel(PasswordManagerEventHandler eventHandler, byte[] key,IPwRepository repository)
+        public PasswordListViewViewModel(PasswordManagerEventHandler eventHandler, byte[] key, IPwRepository repository)
         {
             Passwords = new ObservableCollection<PasswordListItemViewModel>();
             _repository = repository;
@@ -44,7 +68,29 @@ namespace PasswordManager.Models
         {
             foreach (var storedPassword in _repository.GetPasswordEntries())
             {
-                Passwords.Add(new PasswordListItemViewModel(_eventHandler, key, storedPassword));
+                _passwordsBackup.Add(new PasswordListItemViewModel(_eventHandler, key, storedPassword));
+            }
+            //save all the passwordListitem viewmodes for filtering.
+            Passwords = _passwordsBackup;
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private void OnFilterChanged()
+        {
+            if (string.IsNullOrEmpty(Filter))
+            {
+                Passwords = _passwordsBackup;
+            }
+            else
+            {
+                var query = from pwListItemViewModel in _passwordsBackup
+                            where pwListItemViewModel.PasswordEntryModel.Username.Contains(Filter) || pwListItemViewModel.PasswordEntryModel.Website.Contains(Filter)
+                            select pwListItemViewModel;
+                Passwords = new ObservableCollection<PasswordListItemViewModel>(query.ToList());
             }
         }
     }
